0be4edf2ef279c912dce092166136cfe6f46665e
Camilo Esteban Zambrano Pereira <53503084+CamSP@users.noreply.github.com>
2021-04-06 06:59:52 -0500

    Update display.v

--- alu/src/display/display.v
+++ alu/src/display/display.v
@@ -2,47 +2,47 @@
 module display(
     input [15:0] num,
     input clk,
-    output [0:6] sseg,
-    output reg [3:0] an,
-	 input rst
+    output [0:7] sseg,
+    output reg [7:0] an,
+	input rst,
+	input neg
     );
 
 
 
 reg [3:0]bcd=0;
+reg negativo = 0;
  
-BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg));
+BCDtoSSeg bcdtosseg(.BCD(bcd), .SSeg(sseg), .neg(negativo));
 
 reg [26:0] cfreq=0;
 wire enable;
 
 // Divisor de frecuecia
-
 assign enable = cfreq[16];
-assign led =enable;
+
 always @(posedge clk) begin
-  if(rst==1) begin
+  /*if(rst==1) begin
 		cfreq <= 0;
-	end else begin
+	end else begin*/
 		cfreq <=cfreq+1;
-	end
+	//end
 end
 
-reg [1:0] count =0;
+reg [1:0] count = 0;
 always @(posedge enable) begin
 		if(rst==1) begin
-			count<= 0;
-			an<=4'b1111; 
+			an=8'b11111111; 
 		end else begin 
-			count<= count+1;
-			an<=4'b1101; 
-			case (count) 
-				2'h0: begin bcd <= num[3:0];   an<=4'b1110; end 
-				2'h1: begin bcd <= num[7:4];   an<=4'b1101; end 
-				2'h2: begin bcd <= num[11:8];  an<=4'b1011; end 
-				2'h3: begin bcd <= num[15:12]; an<=4'b0111; end 
-			endcase
-		end
+			count <= count+1;
+            case (count) 
+				2'h0: begin negativo = neg; bcd <= num[3:0];    an<=8'b11111110; end 
+				2'h1: begin negativo = 1'b1; bcd <= num[7:4];   an<=8'b11111101; end 
+				2'h2: begin negativo = 1'b1; bcd <= num[11:8];  an<=8'b11111011; end 
+				2'h3: begin negativo = 1'b1; bcd <= num[15:12]; an<=8'b11110111; end 
+            endcase
+	    end
+
 end
 
 endmodule
